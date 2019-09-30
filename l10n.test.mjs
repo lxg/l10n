3b6a4c803ed407af1a459cd57dec6c2af1ef1420
Alexander Günsche <git@lxg.de>
2019-09-30 19:09:49 +0200

    streamline tests

--- l10n.test.mjs
+++ l10n.test.mjs
@@ -1,6 +1,5 @@
 import L10n from "./l10n.mjs"
 
-
 test('constructor', () => {
     const l10n = new L10n({
         "de-DE": {
@@ -13,7 +12,6 @@ test('constructor', () => {
     expect(l10n.catalogs["de-DE"]).toEqual({"abc": "xyz"})
     expect(l10n.fallbacks).toEqual({"de" : "de-DE"})
 })
-
 
 // our permanent instance
 const l10n = new L10n({
@@ -28,64 +26,25 @@ const l10n = new L10n({
     }
 }, "de-DE")
 
-
-// Test setting and getting locale
-
-test('Locale is set to en-GB', () => {
+test('Test setting and getting locale', () => {
     l10n.setLocale("en-GB")
     expect(l10n.locale).toBe("en-GB")
+    expect(l10n.getLocale()).toBe("en-GB")
 })
 
-test('Locale is correctly set and can be retrieved as en-US', () => {
-    l10n.setLocale("en-US")
-    expect(l10n.getLocale()).toBe("en-US")
-})
-
-// Test fully translated language (German)
-
-test('"Hello World" is translated to "Hallo Welt"', () => {
+test('Test fully translated language (German)', () => {
     l10n.setLocale("de-DE")
     expect(l10n.t("Hello World")).toBe("Hallo Welt")
-})
-
-test('"Amount" is translated to "Menge" in the "count" context', () => {
-    l10n.setLocale("de-DE")
     expect(l10n.x("count", "Amount")).toBe("Menge")
-})
-
-test('"Amount" is translated to "Betrag" in the "money" context', () => {
-    l10n.setLocale("de-DE")
     expect(l10n.x("money", "Amount")).toBe("Betrag")
-})
-
-test('"One element" is translated to "Ein Element" if the number is 1', () => {
-    l10n.setLocale("de-DE")
     expect(l10n.n("One element", "%s elements", 1)).toBe("Ein Element")
-})
-
-test('"%s elements" is translated to "%s Elemente" if the number is 3', () => {
-    l10n.setLocale("de-DE")
     expect(l10n.n("One element", "%s elements", 3)).toBe("%s Elemente")
 })
 
-// Test partially translated language (French)
-
-test('"Hello World" is translated to "Bonjour tout le monde"', () => {
+test('Test partially translated language (French)', () => {
     l10n.setLocale("fr-FR")
     expect(l10n.t("Hello World")).toBe("Bonjour tout le monde")
-})
-
-test('"Amount" is returned untranslated due to missing translation', () => {
-    l10n.setLocale("fr-FR")
     expect(l10n.x("money", "Amount")).toBe("Amount")
-})
-
-test('"One element" is returned untranslated due to missing translation', () => {
-    l10n.setLocale("fr-FR")
     expect(l10n.n("One element", "%s elements", 1)).toBe("One element")
-})
-
-test('"%s elements" is returned untranslated due to missing translation', () => {
-    l10n.setLocale("fr-FR")
     expect(l10n.n("One element", "%s elements", 3)).toBe("%s elements")
 })
