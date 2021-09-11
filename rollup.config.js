3f59f91f0e702d3c248a27d71da2327036c6a958
Alexander Günsche <git@lxg.de>
2021-09-11 22:38:00 +0200

    create CommonJS modules during build

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,37 @@
 import { terser } from 'rollup-plugin-terser'
+import resolve from '@rollup/plugin-node-resolve'
 
-export default ['l10n', 'date'].map(name => ({
-    input: `src/${name}.js`,
-    output: {
-        file: `dist/${name}.js`
-    },
-    plugins: [terser({
-        mangle: {
-            properties : {
-                regex: ['^_']
+const builds = []
+
+;['js', 'cjs'].map(type => {
+    ['l10n', 'date'].map(name => {
+        const plugins = []
+
+        plugins.push(resolve())
+
+        plugins.push(terser({
+            mangle: {
+                properties : {
+                    regex: ['^_']
+                }
             }
+        }))
+
+        const config = {
+            input: `src/${name}.js`,
+            output: {
+                file: `dist/${name}.${type}`
+            },
+            plugins
         }
-    })]
-}))
+
+        if (type === "cjs") {
+            config.output.format = "cjs"
+            config.output.exports = "auto"
+        }
+
+        builds.push(config)
+    })
+})
+
+export default builds
